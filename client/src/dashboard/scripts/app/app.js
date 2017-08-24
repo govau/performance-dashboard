d65c8d101e72b08d451cb1f78d98637d1e12418a
Josh Mostafa <micapam@gmail.com>
2017-08-24 17:17:01 +1000

    Single slice trends (#12)

--- client/src/dashboard/scripts/app/app.js
+++ client/src/dashboard/scripts/app/app.js
@@ -3,13 +3,13 @@ import React from 'react';
 import {Provider} from 'react-redux';
 import DashboardShowPage from './pages/dashboardShow';
 
-import {configureDatavizkit} from '@gov.au/datavizkit';
+// import {configureDatavizkit} from '@gov.au/datavizkit/lib/configure';
 
 
 const AppContainer = (props) => {
   const {store, emitter} = props;
 
-  configureDatavizkit({});
+  // configureDatavizkit({});
 
   return (
     <Provider store={store}>
