a613c3dcc362761f41d03993c4731561dcc0c048
Brendan Gordon <brendan.gordon@digital.gov.au>
2018-12-06 14:22:15 +1100

    Disable action view logger by default in prod/cf

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -87,6 +87,11 @@ Rails.application.configure do
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
 
+  # Action view logger generates a lot of log messages, so disable it by default
+  unless ENV["ENABLE_ACTION_VIEW_LOGGER"].present?
+    config.action_view.logger = nil
+  end
+
   # Use a different logger for distributed setups.
   # require 'syslog/logger'
   # config.logger = ActiveSupport::TaggedLogging.new(Syslog::Logger.new 'app-name')
