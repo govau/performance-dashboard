996e29a615fd095bd4dc8b3399a55643e57d0d68
Brendan <2324569+gordcorp@users.noreply.github.com>
2018-12-10 17:15:26 +1100

    Disable action view logger by default in prod/cf (#27)

--- config/environments/cloudfoundry.rb
+++ config/environments/cloudfoundry.rb
@@ -51,6 +51,11 @@ Rails.application.configure do
   # when problems arise.
   config.log_level = :debug
 
+  # Action view logger generates a lot of log messages, so disable it by default
+  unless ENV["ENABLE_ACTION_VIEW_LOGGER"].present?
+    config.action_view.logger = nil
+  end
+
   # Prepend all log lines with the following tags.
   config.log_tags = [ :request_id ]
 
