7df6691b7c53bf207165de81b9980d91a8f88824
Jon Alport <jon.alport@digital.gov.au>
2018-12-20 18:47:08 +0200

    Upgrade Webpack v1 to v3

--- client/config/webpack.config.devserver.js
+++ client/config/webpack.config.devserver.js
@@ -11,77 +11,121 @@ import autoprefixer from 'autoprefixer';
 import * as CONFIG from './_config';
 const projectName = require('./../../package').name;
 
-
 const NODE_ENV = String(process.env.NODE_ENV ? process.env.NODE_ENV : 'development');
 const DEBUG = NODE_ENV === 'development';
 
-
 let webpackConfig = {
 	name: projectName,
-	debug: true,
-  devtool: 'eval', //inline-source-map', //'cheap-module-source-map',
+  devtool: 'eval', 
 	context: CONFIG.DIR_SRC,
-    entry: {
-      ['dashboard']: [`./dashboard`],
-      ['dashboard-index']: [`./dashboard-index`],
-      ['app_shell']: [`./app_shell`],
-      ['editor']: [`./editor`],
-      ['login']: [`./login`],
-    },
+  entry: {
+    ['dashboard']: [`./dashboard`],
+    ['dashboard-index']: [`./dashboard-index`],
+    ['app_shell']: [`./app_shell`],
+    ['editor']: [`./editor`],
+    ['login']: [`./login`],
+  },
 	output: {
 	  path: CONFIG.DIR_DIST,
-    // Add /* filename */ comments to generated require()s in the output.
     pathinfo: true,
-    // This does not produce a real file. It's just the virtual path that is
-    // served by WebpackDevServer in development. This is the JS bundle
-    // containing code from all our entry points, and the Webpack runtime.
-    filename: 'javascripts/[name].js',       // relative     - determines output file
-    // In development, we always serve from the root. This makes config easier.
-    publicPath:  `/`,     // Web root. publicPath + filename must equal resource path in dev server
-    // publicPath:  `http://${CONFIG.WEBPACK_HOST}:${CONFIG.WEBPACK_PORT}/` could be this
+    filename: 'javascripts/[name].js',
+    publicPath:  `/`,
     sourceMapFilename: "javascripts/[name].js.map"
 	},
   externals: {
   },
 	module: {
-    // First, run the linter.
-    // It's important to do this before Babel processes the JS.
-		preLoaders: [
+		rules: [
       {
-        test: /\.(js|jsx)$/,
-        loader: 'eslint',
-        include: CONFIG.DIR_SRC,
-        exclude: [
-          `${CONFIG.DIR_SRC}/dashboard`   // todo - switch on
-        ]
-      }
-		],
-		loaders: [
+        test: /\.js$/,
+        exclude: /node_modules/,
+        enforce: 'pre',
+        use: [
+          'babel-loader',
+          'eslint-loader',
+        ],
+      },      
 			{
-				test: /\.(js|json)$/,
-				loaders: ['babel'],
-				exclude: /node_modules/
-			},
-      // "postcss" loader applies autoprefixer to our CSS.
-      // "css" loader resolves paths in CSS and adds assets as dependencies.
-      // "style" loader turns CSS into JS modules that inject <style> tags.
-      // In production, we use a plugin to extract that CSS to a file, but
-      // in development "style" loader enables hot editing of CSS.
+				test: /\.(js)$/,
+        loaders: ['babel-loader'],
+        exclude: [
+          CONFIG.DIR_NPM,
+          CONFIG.DIR_TEST
+        ],        
+      },
       {
         test: /\.css$/,
-        loader: 'style!css?importLoaders=1!postcss?sourceMap'
+        use: [
+          // 'style-loader',
+          {
+            loader: 'css-loader',
+            options: {
+              sourceMap: true,
+              importLoaders: 1,
+              url: false,
+              minimize: true
+              // modules: true
+            }
+          },
+          {
+            loader: 'postcss-loader',
+            options: { sourceMap: true }
+          },
+          {
+            loader: 'resolve-url-loader',
+            options: {
+              attempts: 1,
+              debug: true,
+              sourceMap: false,
+            }
+          }
+        ]
       },
-			{
-        test: /\.(scss)$/,
-				loader: 'style!css?&sourceMap!postcss!resolve-url!sass?sourceMap'
-			},
+      {
+        test: /\.scss$/,
+        use: [
+          // 'style-loader',
+          {
+            loader: 'css-loader',
+            options: {
+              sourceMap: true,
+              importLoaders: 1,
+              url: false,
+              minimize: true
+              // modules: true
+            },
+          },
+          {
+            loader: 'postcss-loader',
+            options: { sourceMap: true }
+          },
+          {
+            loader: 'resolve-url-loader',
+            options: {
+              attempts: 1,
+              debug: true,
+              sourceMap: false,
+            },
+          },
+          {
+            loader: 'sass-loader',
+            options: {
+              sourceMap: true,
+              data: `$env: ${NODE_ENV};`,
+              includePaths: [
+                CONFIG.DIR_SRC,
+                CONFIG.DIR_NPM
+              ]
+            }
+          }
+        ]
+      },
       {
         test: /\.(jpe?g|gif|png|svg)$/,
         loaders: [
-          "file?name=images/[name].[ext]?[hash]"
-          // 'image-webpack?{progressive:true, optimizationLevel: 7, interlaced: false, pngquant:{quality: "65-90", speed: 4}}',
+          'file-loader?name=/images/[name].[ext]?[hash]'
         ]
-      }
+      }      
 		]
 	},
 	plugins: [
@@ -94,30 +138,13 @@ let webpackConfig = {
 		new BellOnBundlerErrorPlugin()
 	],
 	resolve: {
-		extensions : ['', '.js', '.scss']
+		extensions : ['.js', '.scss']
 	},
-  // Some libraries import Node modules but don't use them in the browser.
-  // Tell Webpack to provide empty mocks for them so importing them works.
   node: {
     fs: 'empty',
     net: 'empty',
     tls: 'empty'
-  },
-
-  postcss : [
-    autoprefixer()
-  ],
-	sassLoader: {
-		precision: 8,
-		includePaths: [
-			CONFIG.DIR_SRC,
-			CONFIG.DIR_NPM
-		],
-		sourceMap: true,
-		lineNumbers: true,
-		bundleExec: true,
-		data: `$env: ${NODE_ENV};`
-	}
+  }
 };
 
 export default webpackConfig;
--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -1,24 +1,27 @@
-// Load environment variables from .env file. Suppress warnings using silent
-// if this file is missing. dotenv will never modify any environment variables
-// that have already been set.
-// https://github.com/motdotla/dotenv
-require('dotenv').config({silent: true});
+/*
+  Load environment variables from .env file. Suppress warnings using silent
+  if this file is missing. dotenv will never modify any environment variables
+  that have already been set.
+
+  https://github.com/motdotla/dotenv
+*/
+
+require('dotenv').config({ silent: true });
 
 import webpack from 'webpack';
 import ExtractTextPlugin from 'extract-text-webpack-plugin';
 import BellOnBundlerErrorPlugin from 'bell-on-bundler-error-plugin';
-import autoprefixer from 'autoprefixer';
-import {BundleAnalyzerPlugin} from 'webpack-bundle-analyzer'
+// import autoprefixer from 'autoprefixer';
+import { BundleAnalyzerPlugin } from 'webpack-bundle-analyzer'
 import * as CONFIG from './client/config/_config';
 
 const GitRevisionPlugin = require('git-revision-webpack-plugin')
-const RollbarSourceMapPlugin = require('rollbar-sourcemap-webpack-plugin')
+// const RollbarSourceMapPlugin = require('rollbar-sourcemap-webpack-plugin')
 const projectName = require('./package').name;
 const NODE_ENV = String(process.env.NODE_ENV ? process.env.NODE_ENV : 'production');
 const DEBUG = NODE_ENV === 'development';
 const showVisualisation = false;
 const assetsBaseUrl = 'https://dashboard.gov.au';
-const fs = require('fs');
 const revision = require('child_process').execSync('git rev-parse HEAD').toString().trim();
 
 if (!DEBUG) {
@@ -34,35 +37,24 @@ console.log(`VERSION: ${revision}`);
 console.log(`DIR DIST: ${CONFIG.DIR_DIST}`);
 console.log(`ASSETS BASE URL: ${assetsBaseUrl}`);
 
-// var shouldUseRelativeAssetPaths = './';
-// // Note: defined here because it will be used more than once.
-// const cssFilename = 'stylesheets/[name]-mcss.css';
-//
-// // ExtractTextPlugin expects the build output to be flat.
-// // (See https://github.com/webpack-contrib/extract-text-webpack-plugin/issues/27)
-// // However, our output is structured with css, js and media folders.
-// // To have this structure working with relative paths, we have to use custom options.
-// const extractTextPluginOptions = shouldUseRelativeAssetPaths
-//   // Making sure that the publicPath goes back to to build folder.
-//   ? { publicPath: Array(cssFilename.split('/').length).join('../') }
-//   : undefined;
-
-let ExtractMcss = new ExtractTextPlugin("stylesheets/[name]-mcss.css", {
-  publicPath: CONFIG.DIR_DIST,
+let ExtractMcss = new ExtractTextPlugin({
+  filename: 'stylesheets/[name]-mcss.css',
+  // publicPath: CONFIG.DIR_DIST,
+  publicPath: assetsBaseUrl,
   allChunks: false
 });
 
-let ExtractSass = new ExtractTextPlugin("stylesheets/[name].css", {
-  publicPath: CONFIG.DIR_DIST,
+let ExtractSass = new ExtractTextPlugin({
+  filename: 'stylesheets/[name].css',
+  // publicPath: CONFIG.DIR_DIST,
+  publicPath: assetsBaseUrl,
   allChunks: false
 });
 
 let webpackConfig = {
   name: projectName,
-  // don't attempt to continue if there are errors
   bail: true,
-  debug: DEBUG,
-  devtool: 'source-map',
+  devtool: 'hidden-source-map',
   target: 'web',
   context: CONFIG.DIR_SRC,
   entry: {
@@ -75,82 +67,114 @@ let webpackConfig = {
   output: {
     filename: 'javascripts/[name].js',
     chunkFilename: 'javascripts/[name].js',
-    path: CONFIG.DIR_DIST,    // absolute - determines output dir
-    sourceMapFilename: "javascripts/[name].js.map"
-  },
-  // When you want to import a global API into the bundle
-  externals: {
+    path: CONFIG.DIR_DIST, // absolute - determines output dir
+    sourceMapFilename: 'javascripts/[name].js.map'
   },
   module: {
-    // Pre-process files as they are loaded by "import" statements.
-    // Similar to a gulp task.
-    // Chains are applied right to left,
-    // Can accept options by query params or by object syntax below,
-    // see "loader options"
-    // First, run the linter.
-    // It's important to do this before Babel processes the JS.
-    preLoaders: [
+    rules: [
       {
-        test: /\.(js|jsx)$/,
-        loader: 'eslint',
-        include: CONFIG.DIR_SRC,
-      }
-    ],
-    loaders: [
+        test: /\.js$/,
+        exclude: /node_modules/,
+        enforce: 'pre',
+        use: [
+          'babel-loader',
+          'eslint-loader'
+        ],
+      },
       {
         test: /\.(js)$/,
-        loaders: ['babel'],
+        loaders: ['babel-loader'],
         exclude: [
           CONFIG.DIR_NPM,
           CONFIG.DIR_TEST
         ],
       },
       {
-        test: /\.json$/,
-        loader: 'json'
-      },
-      // The notation here is somewhat confusing.
-      // "postcss" loader applies autoprefixer to our CSS.
-      // "css" loader resolves paths in CSS and adds assets as dependencies.
-      // "style" loader normally turns CSS into JS modules injecting <style>,
-      // but unlike in development configuration, we do something different.
-      // `ExtractTextPlugin` first applies the "postcss" and "css" loaders
-      // (second argument), then grabs the result CSS and puts it into a
-      // separate file in our build process. This way we actually ship
-      // a single CSS file in production instead of JS code injecting <style>
-      // tags. If you use code splitting, however, any async bundles will still
-      // use the "style" loader inside the async code so CSS from them won't be
-      // in the main CSS file.
-      {
         test: /\.css$/,
-        loader: ExtractMcss.extract(
-          'style',
-          'css?importLoaders=1!postcss?sourceMap!postcss',
-          // extractTextPluginOptions
-        )
-        // Note: this won't work without `new ExtractTextPlugin()` in `plugins`.
+        use: ExtractMcss.extract({
+          fallback: 'style-loader',
+          use: [
+            {
+              loader: 'css-loader',
+              options: {
+                sourceMap: true,
+                importLoaders: 1,
+                minimize: true
+                // modules: true
+              }
+            },
+            {
+              loader: 'postcss-loader',
+              options: { sourceMap: true }
+            },
+            {
+              loader: 'resolve-url-loader',
+              options: {
+                debug: false,
+                sourceMap: true
+              },
+            }
+          ]
+        })
       },
       {
-        test: /\.(scss)$/,
-        loader: ExtractSass.extract(
-          'style', `css?${DEBUG ? 'sourceMap': ''}!postcss!resolve-url!sass?sourceMap`)
+        test: /\.scss$/,
+        use: ExtractSass.extract({
+          fallback: 'style-loader',
+          use: [
+            {
+              loader: 'css-loader',
+              options: {
+                sourceMap: true,
+                importLoaders: 1,
+                minimize: true
+                // modules: true
+              }
+            },
+            {
+              loader: 'postcss-loader',
+              options: { sourceMap: true }
+            },
+            {
+              loader: 'resolve-url-loader',
+              options: {
+                debug: false,
+                sourceMap: true
+              },
+            },
+            {
+              loader: 'sass-loader',
+              options: {
+                sourceMap: true,
+                sourceMapContents: false,
+                data: `$env: ${NODE_ENV};`,
+                includePaths: [
+                  CONFIG.DIR_SRC,
+                  CONFIG.DIR_NPM
+                ]
+              }
+            }
+          ]
+        })
       },
       {
         test: /\.(jpe?g|gif|png|svg)$/,
         loaders: [
-          "file?name=/images/[name].[ext]?[hash]"
-          // 'image-webpack?{progressive:true, optimizationLevel: 7, interlaced: false, pngquant:{quality: "65-90", speed: 4}}',
+          'file-loader?name=/images/[name].[ext]?[hash]'
         ]
       }
     ]
   },
-  // Add functionality typically related to bundles in webpack
   plugins: [
-    new RollbarSourceMapPlugin({
-      accessToken: process.env.ROLLBAR_ACCESS_TOKEN_SERVER,
-      version: revision,
-      publicPath: assetsBaseUrl
+    new webpack.SourceMapDevToolPlugin({
+      filename: '[file].map',
+      publicPath: `${assetsBaseUrl}/`,
     }),
+    // new RollbarSourceMapPlugin({
+    //   accessToken: process.env.ROLLBAR_ACCESS_TOKEN_SERVER,
+    //   version: revision,
+    //   publicPath: assetsBaseUrl
+    // }),
     new GitRevisionPlugin(),
     new webpack.DefinePlugin({
       'process.env': {
@@ -163,71 +187,33 @@ let webpackConfig = {
     ExtractMcss,
     new webpack.optimize.CommonsChunkPlugin({
       name: 'editor-vendor',
-      chunks: ["editor"],
+      chunks: ['editor'],
       minChunks(module, count) {
         var context = module.context;
         return context && [
-            'node_modules/react',
-            'node_modules/react-dom',
-            'node_modules/react-redux',
-            'node_modules/redux',
-            'node_modules/redux-jsonschema-form',
-            'node_modules/jsonschema',
-            'node_modules/react-router',
-            'node_modules/history',
-            'node_modules/validator',
-            'node_modules/lodash'
-          ].find(substr => context.indexOf(substr) >= 0);
+          'node_modules/react',
+          'node_modules/react-dom',
+          'node_modules/react-redux',
+          'node_modules/redux',
+          'node_modules/redux-jsonschema-form',
+          'node_modules/jsonschema',
+          'node_modules/react-router',
+          'node_modules/history',
+          'node_modules/validator',
+          'node_modules/lodash'
+        ].find(substr => context.indexOf(substr) >= 0);
       }
     })
   ],
-  // Options affecting the resolving of modules
   resolve: {
-    extensions : ['', '.js', '.scss'],
+    extensions : ['.js', '.scss'],
   },
-  // Some libraries import Node modules but don't use them in the browser.
-  // Tell Webpack to provide empty mocks for them so importing them works.
   node: {
     fs: 'empty',
     net: 'empty',
     tls: 'empty'
-  },
-
-
-  //
-  // loader options
-  //
-
-  postcss : [
-    autoprefixer()
-  ],
-  sassLoader: {
-    precision: 8,
-    includePaths: [
-      CONFIG.DIR_SRC,
-      CONFIG.DIR_NPM
-    ],
-    sourceMap: DEBUG,
-    lineNumbers: DEBUG,
-    bundleExec: true,
-    data: `$env: ${NODE_ENV};`
-  },
-
+  }
 };
-
-if (!DEBUG) {
-  webpackConfig.plugins.push(
-    // Merge all duplicate modules
-    new webpack.optimize.DedupePlugin(),
-    new webpack.optimize.OccurenceOrderPlugin(),
-    new webpack.optimize.UglifyJsPlugin({
-      compress: {
-        warnings: false,
-        // drop_debugger: false
-      },
-    })
-  )
-}
 
 if (showVisualisation) {
   webpackConfig.plugins.push(new BundleAnalyzerPlugin({
