e87e451c4bedb00fab7b57c20e41ce05d5cee11e
Jon Alport <jon@jonalport.com>
2019-01-15 15:38:04 +1100

    Upgrade Webpack v1 to v3 (#29)

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,18 +6,5 @@ module.exports = {
   },
   "rules": {
     "eqeqeq": 0
-    // // Best Practices
-    // // disallow the use of alert, confirm, and prompt
-    // "no-alert": 1,
-    // // disallow the use of console
-    // "no-console": 0,
-    // // no undefined
-    // "no-undef": 1,
-    //
-    // // Stylistic
-    // // template strings must be declared with backticks
-    // "quotes": [1, "single"],
-    // // require or disallow use of semicolons instead of ASI
-    // "semi": 1,
   }
 };
